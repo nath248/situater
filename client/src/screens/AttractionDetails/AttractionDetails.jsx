ab06f6260e772c3aa514511b174f1f4de131792d
natherrera248 <nmherrera20@outlook.com>
2022-01-27 15:48:51 -0500

    update detail pages

--- client/src/screens/AttractionDetails/AttractionDetails.jsx
+++ client/src/screens/AttractionDetails/AttractionDetails.jsx
@@ -30,7 +30,7 @@ function AttractionDetails() {
         <h3>Check Out All This Cool Attraction!</h3>
         <h3>Explore New Horizons!</h3>
             <div className='attraction-details-info-box'>
-              <p>location name</p>
+          <p>{attraction.location.name}</p>
               <p>{attraction.name}</p>
               <p>{attraction.type}</p>
               <p>{attraction.price}</p>
