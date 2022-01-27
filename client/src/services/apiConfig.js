02f78cdee60995af966019370e8f250b72dcd7ff
natherrera248 <nmherrera20@outlook.com>
2022-01-27 14:30:25 -0500

    update config

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-// const getToken = () => {
-//   return new Promise(resolve => {
-//       resolve(`Bearer ${localStorage.getItem('token') || null}`)
-//   })
-// }
+const getToken = () => {
+  return new Promise(resolve => {
+      resolve(`Bearer ${localStorage.getItem('token') || null}`)
+  })
+}
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
