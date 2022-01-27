733d023637d0372dbc8c910f7ebb0b4d23aadeb0
natherrera248 <nmherrera20@outlook.com>
2022-01-27 09:59:21 -0500

    update to models

--- back-end/situater/models.py
+++ back-end/situater/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Location(models.Model):
-  name = models.CharField(max_length=256)
-  image = models.CharField(max_length=600)
+  name = models.CharField(max_length=800)
+  image = models.CharField(max_length=800)
   
   def __str__(self):
         return self.name
   
 class Attraction(models.Model):
-    name = models.CharField(max_length=256)
-    image = models.CharField(max_length=600)
-    type = models.CharField(max_length=126)
+    name = models.CharField(max_length=800)
+    image = models.CharField(max_length=800)
+    type = models.CharField(max_length=800)
     price = models.PositiveIntegerField(default=0)
-    rating = models.CharField(max_length=126)
+    rating = models.CharField(max_length=800)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='attractions')
 
     def __str__(self):
