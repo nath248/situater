afafd4d8a1e7984ad6c99879cd3fff4fe683d73c
natherrera248 <nmherrera20@outlook.com>
2022-01-28 14:17:35 -0500

    add nav/footer css

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,20 @@
+import './Footer.css'
+
 function Footer() {
-  return <div></div>;
+  
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer-main">
+      <div className="icons">
+        <a href="https://linkedin.com/in/nath248/ " target="_blank" rel="noreferrer"><img src="https://res.cloudinary.com/dojhf40bp/image/upload/v1643394622/linkedin-icon_b0zg03.png" alt="LinkedIn Icon" /></a>
+        <a href="https://github.com/nath248" target="_blank" rel="noreferrer"><img src="https://res.cloudinary.com/dojhf40bp/image/upload/v1643394586/github-icon_zifws7.png" alt="GitHub Icon" /></a>
+      </div>
+      <div className="footer-copyright">
+        <small>&copy; Copyright {currentYear}, Nathaly Herrera</small>
+      </div>
+    </div>
+  )
 }
 
 export default Footer;
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,5 +1,36 @@
-function Nav() {
-  return <div></div>;
+import './Nav.css'
+import { Link, NavLink } from "react-router-dom";
+
+const authenticatedOptions = (
+  <>
+      <NavLink className="navlinks" to="/add-location">Add Location </NavLink>
+      <NavLink className="navlinks" to="/add-attraction">Add Attraction </NavLink>
+      <NavLink className="navlinks" to="/logout">Log Out</NavLink>
+  </>
+)
+const unauthenticatedOptions = (
+  <>
+      <NavLink className="navlinks" to="/signup">Sign Up/Log In</NavLink>
+      {/* <NavLink className="navlinks" to="/login">Log In</NavLink> */}
+  </>
+)
+const alwaysOptions = (
+  <>
+      <NavLink className="navlinks" to="/locations">Locations </NavLink>
+      <NavLink className="navlinks" to="/attractions">Attractions </NavLink>
+  </>
+)
+
+function Nav({user}) {
+  return (
+    <div className='nav-main'>
+      <Link to="/" className="nav-name">SITUATER</Link>
+      <div className="nav-links">
+        {alwaysOptions}
+        {user ? authenticatedOptions : unauthenticatedOptions}
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
