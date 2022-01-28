ac87b6de84a6cd898cdd6b0ee7cb308b30f07eb6
natherrera248 <nmherrera20@outlook.com>
2022-01-28 11:15:59 -0500

    update

--- client/src/screens/AttractionDetails/AttractionDetails.jsx
+++ client/src/screens/AttractionDetails/AttractionDetails.jsx
@@ -1,64 +1,61 @@
-import './AttractionDetails.css'
-import { useState, useEffect } from 'react'
-import { getAttraction, deleteAttraction } from '../../services/attractions'
-import { getLocation } from '../../services/locations'
-import { useParams, Link } from 'react-router-dom'
+import "./AttractionDetails.css";
+import { useState, useEffect } from "react";
+import { getAttraction, deleteAttraction } from "../../services/attractions";
+import { getLocation } from "../../services/locations";
+import { useParams, Link } from "react-router-dom";
 
 function AttractionDetails(props) {
-  const [attraction, setAttraction] = useState(null)
-  const [location, setLocation] = useState(null)
-  const [isLoaded, setLoaded] = useState(false)
-  const { id } = useParams()
+  const [attraction, setAttraction] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchAttraction = async () => {
-      const attraction = await getAttraction(id)
-      setAttraction(attraction)
-      setLoaded(true)
-    }
-    fetchAttraction()
-  }, [id])
+      const attraction = await getAttraction(id);
+      setAttraction(attraction);
+      setLoaded(true);
+    };
+    fetchAttraction();
+  }, [id]);
 
   useEffect(() => {
     const fetchLocation = async () => {
-      const location = await getLocation(id)
-      setLocation(location)
-      setLoaded(true)
-    }
-    fetchLocation()
-  }, [id])
+      const location = await getLocation(id);
+      setLocation(location);
+      setLoaded(true);
+    };
+    fetchLocation();
+  }, [id]);
 
   if (!isLoaded) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
-
-
   return (
-    <div className='attraction-details-main'>
-      <div className='attraction-details-img'>
+    <div className="attraction-details-main">
+      <div className="attraction-details-img">
         <h3>Attraction Name</h3>
       </div>
-      <div className='attraction-details-info'>
+      <div className="attraction-details-info">
         <h3>Check Out All This Cool Attraction!</h3>
         <h3>Explore New Horizons!</h3>
-        <div className='attraction-details-info-box'>
-          {location.attractions.length && location.attractions.map(list => {
-            return list === attraction.name ?
-            <p>{location.name}</p>: null
-          })
-            }
-              <p></p>
-              <p>{attraction.name}</p>
-              <p>{attraction.type}</p>
-              <p>{attraction.price}</p>
-              <p>{attraction.rating} rating</p>
-            </div>
+        <div className="attraction-details-info-box">
+          {location.attractions.length &&
+            location.attractions.map((list) => {
+              return list === attraction.name ? <p>{location.name}</p> : null;
+            })}
+          <p></p>
+          <p>{attraction.name}</p>
+          <p>{attraction.type}</p>
+          <p>{attraction.price}</p>
+          <p>{attraction.rating} rating</p>
+        </div>
         <Link to={`/attractions/${attraction.id}/edit`}>EDIT</Link>
         <button onClick={() => deleteAttraction(attraction.id)}>DELETE</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default AttractionDetails;
